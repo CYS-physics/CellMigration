5693f3b66a573ea3dabd595c85cc0b4af4409775
CYS-physics <219toto@snu.ac.kr>
2021-07-18 19:33:06 +0900

    added boundary condition

--- Cell_Lab.py
+++ Cell_Lab.py
@@ -1,4 +1,5 @@
 # asymmetric interacting particles under active noise
+# 2D periodic boundary condition
 # Yunsik Choe, Seoul national University
 
 import numpy as np                   # numerical calculation
@@ -11,15 +12,15 @@ import sys
 
 
 class Cell_Lab:     # OOP
-    """basic model to simulate passive objects under active noise"""
+    """basic model to simulate 2D passive objects under active noise"""
     
     # initializing coefficients of model and configuration of states in phase space
     
-    def __init__(self):
+    def __init__(self,L, N_ptcl,Fs):
         
         
         # set up coefficients
-        self.set_coeff(alpha, u, len_time, N_time, N_X,N_ptcl,v,mu, muw) 
+        self.set_coeff(L,N_ptcl,Fs) 
       
         # initializing configuration of state
         self.set_zero()
@@ -29,8 +30,12 @@ class Cell_Lab:     # OOP
             
             
     # setting coefficients
-    def set_coeff(self):
-        
+    def set_coeff(self,L,N_ptcl,Fs):
+        self.L=L
+        self.N_ptcl=N_ptcl
+        self.Fs=Fs
+        self.dt = 1/Fs
+                  
         
         
         
@@ -44,28 +49,19 @@ class Cell_Lab:     # OOP
         
     # Dynamics part
     def set_zero(self):              # initializing simulation configurations
-        self.time = np.linspace(0,self.len_time,num=self.N_simul)
+        self.etaX
+        self.etaY
         self.s = np.random.choice([1,-1],np.array([self.N_ptcl,self.N_X]))             # random direction at initial time
         self.x = np.random.uniform(-self.L/2, self.L/2, size=np.array([self.N_ptcl,self.N_X]))     # starting with uniformly distributed particles
         self.X = np.zeros(self.N_X)
 #         self.v = np.zeros(self.N_X)
     
-    def tumble(self):             # random part of s dynamics
+    def (self):             # random part of s dynamics
         tumble = np.random.choice([1,-1], size=np.array([self.N_ptcl,self.N_X]), p = [1-self.delta_time*self.alpha/2, self.delta_time*self.alpha/2]) # +1 no tumble, -1 tumble
         return tumble
     
     def dynamics(self,x,s):         # dynamics of x and s to give time difference of x and s
-        dxa = self.u*self.delta_time
-        ds = self.tumble()
-        
-        dx = dxa*s  -self.mu*self.partial_V(x)*self.delta_time
-        if self.model==3:
-            v = self.muw*np.sum(self.partial_V(x),axis=0)
-        elif self.model ==2:
-            v = self.v
-
-
-        return (v, dx, ds)
+      
         
     def time_evolve(self):
         (v, dx, ds) = self.dynamics(self.x, self.s)
