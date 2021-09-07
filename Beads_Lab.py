bd27ebd46ac4816080374372f6ee337046012b56
CYS-physics <219toto@snu.ac.kr>
2021-09-07 14:37:20 +0900

    ensemble

--- Beads_Lab.py
+++ Beads_Lab.py
@@ -13,11 +13,11 @@ class Beads:     # OOP
     
     # initializing coefficients of model and configuration of states in phase space
     
-    def __init__(self,L, N_ptcl,N_active,Fs):
+    def __init__(self,L, N_ptcl,N_active,N_ensemble,Fs):
         
         
         # set up coefficients
-        self.set_coeff(L,N_ptcl,N_active,Fs) 
+        self.set_coeff(L,N_ptcl,N_active,N_ensemble,Fs) 
       
         # initializing configuration of state
         self.set_zero()
@@ -28,12 +28,13 @@ class Beads:     # OOP
             
             
     # setting coefficients
-    def set_coeff(self,L,N_ptcl,N_active,Fs):
+    def set_coeff(self,L,N_ptcl,N_active,N_ensemble,Fs):
         
         # system coefficients
         self.L=L
         self.N_ptcl = N_ptcl
         self.N_active = N_active
+        self.N_ensemble = N_ensemble
         self.Fs=Fs
         self.dt = 1/Fs
         
@@ -67,13 +68,13 @@ class Beads:     # OOP
     # Dynamics part
     def set_zero(self):              # initializing simulation configurations
         
-        self.X = np.linspace(0,self.L,self.N_ptcl+1)[:self.N_ptcl]
-        self.O = np.ones(self.N_active)*np.pi/2
+        self.X = np.ones(self.N_ensemble).reshape(-1,1)*np.linspace(0,self.L,self.N_ptcl+1)[:self.N_ptcl].reshape(1,-1)
+        self.O = np.ones((self.N_ensemble,self.N_active))*np.pi/2
         
         self.set_structure()
         
     def set_structure(self):
-        self.Xs = self.X[1:self.N_active+1]+self.l*np.cos(self.O)   # 1~N_active
+        self.Xs = self.X[:,1:self.N_active+1]+self.l*np.cos(self.O)   # 1~N_active
         self.Ys = self.l*np.sin(self.O)
         
         self.Xs = self.periodic(self.Xs)
@@ -89,22 +90,22 @@ class Beads:     # OOP
         r = np.sqrt(rx**2 + ry**2)
         force = 4*self.k*(-12*r**(-13)/r_0**(-12)+6*r**(-7)/r_0**(-6))*(np.abs(r)<r_cut)
 #         return force*np.divide(ry,r,out=np.zeros_like(ry),where=r!=0)
-        return force*(rx/r,ry/r)
+        return force*(np.divide(rx,r,out=np.zeros_like(rx),where=r!=0),np.divide(ry,r,out=np.zeros_like(ry),where=r!=0))
         
     
     def force(self):    # force from WCA potential (truncated LJ potential -> hard wall repulsion)
-        f1x = np.zeros(self.N_ptcl)
-        f2x = np.zeros(self.N_active)
+        f1x = np.zeros((self.N_ensemble,self.N_ptcl))
+        f2x = np.zeros((self.N_ensemble,self.N_active))
 #         f1y = np.zeros(self.N_active)
-        f2y = np.zeros(self.N_active)
+        f2y = np.zeros((self.N_ensemble,self.N_active))
         
         # particle 1 constrained to wall, particle 2 rotating
         
         # relative position from -> to : r(from)-r(to)
         
         # 1->1
-        relx_right = self.periodic(np.roll(self.X,-1)-self.X)
-        relx_left = self.periodic(np.roll(self.X,1)-self.X)
+        relx_right = self.periodic(np.roll(self.X,-1,axis=1)-self.X)
+        relx_left = self.periodic(np.roll(self.X,1,axis=1)-self.X)
         
         (fx,fy)=self.WCA(relx_right,0,self.r_cut[0])
         f1x += fx # right particle
@@ -112,8 +113,8 @@ class Beads:     # OOP
         f1x += fx  # left particle
         
         # 1->2
-        relx_right = self.periodic(self.X[2:self.N_active+2]-self.Xs)
-        relx_left = self.periodic(self.X[:self.N_active]-self.Xs)
+        relx_right = self.periodic(self.X[:,2:self.N_active+2]-self.Xs)
+        relx_left = self.periodic(self.X[:,:self.N_active]-self.Xs)
         rely_right = -self.Ys
         rely_left = -self.Ys
         
@@ -125,36 +126,35 @@ class Beads:     # OOP
         f2y+=fy
         
         # 2->1
-        relx_right = self.periodic(self.Xs-self.X[:self.N_active])
-        relx_left = self.periodic(self.Xs-self.X[2:self.N_active+2])
+        relx_right = self.periodic(self.Xs-self.X[:,:self.N_active])
+        relx_left = self.periodic(self.Xs-self.X[:,2:self.N_active+2])
         rely_right = self.Ys
         rely_left = self.Ys
         
         (fx,fy)=self.WCA(relx_right,rely_right,self.r_cut[1])
-        f1x[:self.N_active]+=fx
+        f1x[:,:self.N_active]+=fx
         (fx,fy)=self.WCA(relx_left,rely_left,self.r_cut[1])
-        f1x[2:self.N_active+2]+=fx
+        f1x[:,2:self.N_active+2]+=fx
         
         
         # 2->2
-        relx_right = self.periodic(self.Xs[1:]-self.Xs[:-1])
-        relx_left = self.periodic(self.Xs[:-1]-self.Xs[1:])
-        rely_right = self.Ys[1:]-self.Ys[:-1]
-        rely_left = self.Ys[:-1]-self.Ys[1:]
+        relx_right = self.periodic(self.Xs[:,1:]-self.Xs[:,:-1])
+        relx_left = self.periodic(self.Xs[:,:-1]-self.Xs[:,1:])
+        rely_right = self.Ys[:,1:]-self.Ys[:,:-1]
+        rely_left = self.Ys[:,:-1]-self.Ys[:,1:]
         
         (fx,fy)=self.WCA(relx_right,rely_right,self.r_cut[2])
-        f2x[:-1]+=fx
-        f2y[:-1]+=fy
+        f2x[:,:-1]+=fx
+        f2y[:,:-1]+=fy
         (fx,fy)=self.WCA(relx_left,rely_left,self.r_cut[2])
-        f2x[1:]+=fx
-        f2y[1:]+=fy
+        f2x[:,1:]+=fx
+        f2y[:,1:]+=fy
         
         
         # noise
-        f1x+=np.random.normal(0,np.sqrt(2*self.D/self.dt),self.N_ptcl)
-        f2x+=np.random.normal(0,np.sqrt(2*self.D/self.dt),self.N_active)
-        f2y+=np.random.normal(0,np.sqrt(2*self.D/self.dt),self.N_active)
-        
+        f1x+=np.random.normal(0,np.sqrt(2*self.D/self.dt),(self.N_ensemble,self.N_ptcl))
+        f2x+=np.random.normal(0,np.sqrt(2*self.D/self.dt),(self.N_ensemble,self.N_active))
+        f2y+=np.random.normal(0,np.sqrt(2*self.D/self.dt),(self.N_ensemble,self.N_active))
         
         # normal force
 #         fN = self.p*np.sin(self.O)-f1y
@@ -168,21 +168,26 @@ class Beads:     # OOP
         # compute force & torque
         (f1x,f2x,f2y) = self.force()
         Fx = f1x
-        self.v = np.mean(Fx)*self.mu
-        Fx[1:self.N_active+1]+=f2x-self.p*np.cos(self.O)
-        Torque = self.l/2*(f2y*np.cos(self.O)+(f1x[1:self.N_active+1]-f2x-self.p*np.cos(self.O))*np.sin(self.O))
+        Fx[:,1:self.N_active+1]+=f2x-self.p*np.cos(self.O)
+        
+        self.v = np.mean(Fx,axis=1)*self.mu
+        
+        Torque = self.l/2*(f2y*np.cos(self.O)+(f1x[:, 1:self.N_active+1]-f2x-self.p*np.cos(self.O))*np.sin(self.O))
         # update configuration
         self.X+=self.mu*Fx*self.dt
         self.O+=self.mur*Torque*self.dt
 
         self.X = self.periodic(self.X)
-        self.O = np.amax(np.vstack([self.O,np.zeros(self.N_active)]),axis=0)
-        self.O = np.amin(np.vstack([self.O,(np.ones(self.N_active)*np.pi)]),axis=0)
+        self.O = np.amax(np.vstack([[self.O],[np.zeros((self.N_ensemble,self.N_active))]]),axis=0)
+        self.O = np.amin(np.vstack([[self.O],[(np.ones((self.N_ensemble,self.N_active))*np.pi)]]),axis=0)  
+
+
         self.set_structure()
         
     
         
     def animate(self,N_iter,directory):
+        self.set_zero()
         self.time_evolve()
         
         axrange = [-self.L/2, self.L/2, -self.L/100, self.L/10]
@@ -190,8 +195,8 @@ class Beads:     # OOP
         #Setup plot for updated positions
         fig1 = plt.figure()
         ax1 = fig1.add_subplot(111)
-        ax1.scatter(self.X,np.zeros(self.N_ptcl),color='red')
-        ax1.scatter(self.Xs,self.Ys,color='blue')
+        ax1.scatter(self.X[0],np.zeros(self.N_ptcl),color='red')
+        ax1.scatter(self.Xs[0],self.Ys[0],color='blue')
         ax1.axis(axrange)
         ax1.set_aspect('equal','box')
         fig1.show()
@@ -205,8 +210,9 @@ class Beads:     # OOP
         for nn in trange(N_iter):
             
             ax1.clear()
-            ax1.scatter(self.X,np.zeros(self.N_ptcl),s=self.r_cut[0]**2*50000/self.L**2,color='red')
-            ax1.scatter(self.Xs,self.Ys,s=self.r_cut[1]**2*50000/self.L**2,color='blue')
+            ax1.scatter(self.X[0],np.zeros(self.N_ptcl),s=self.r_cut[0]**2*50000/self.L**2,color='red')
+            
+            ax1.scatter(self.Xs[0],self.Ys[0],s=self.r_cut[1]**2*50000/self.L**2,color='blue')
             ax1.axis(axrange)
             ax1.set_aspect('equal', 'box')
             fig1.canvas.draw()
@@ -215,15 +221,13 @@ class Beads:     # OOP
             for _ in range(self.N_skip):
                 self.time_evolve()
                 
-    def measure(self,N_iter,N_repeat,directory):
-        v = np.zeros(N_repeat)
-        for i in trange(N_repeat):
-            self.set_zero
-            v_temp = np.zeros(N_iter)
-            for j in range(N_iter):
-                self.time_evolve()
-                v_temp[j] = np.abs(self.v)
-            v[i] = np.average(v_temp)
+    def measure(self,N_iter):
+        self.set_zero
+        v_traj = np.zeros((self.N_ensemble,N_iter))
+        for j in range(N_iter):
+            self.time_evolve()
+            v_traj[:,j] = self.v
+        v = np.abs(np.average(v_traj,axis=1))
         return v
         
                 
